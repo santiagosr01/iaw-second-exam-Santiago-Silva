db0fc78bf2dfc8af4cefc1d3c2f5ca6017446894
Santiago <sansamuel2208@gmail.com>
2024-02-26 13:04:37 +0100

    Constantes de los libros

--- src/handlers/put-item.mjs
+++ src/handlers/put-item.mjs
@@ -22,7 +22,12 @@ export const putItemHandler = async (event) => {
     // Get id and name from the body of the request
     const body = JSON.parse(event.body);
     const id = body.id;
-    const name = body.name;
+    const disponible = body.disponible;
+    const anoPublicacion = body.anoPublicacion;
+    const autor = body.autor;
+    const editorial = body.editorial;
+    const nombreLibro  = body.nombreLibro;
+    
 
     // Creates a new item, or replaces an old item with a new item
     // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#put-property
